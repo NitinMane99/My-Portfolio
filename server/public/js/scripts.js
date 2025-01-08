b6c7e7c93d02771423748878911a26482f64ab7c
superEarth999 <earthmernstack@gmail.com>
2025-01-08 15:05:56 +0530

    added logo and mobile numbile field edited

--- server/public/js/scripts.js
+++ server/public/js/scripts.js
@@ -2,6 +2,28 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('JavaScript is loaded');
 });
 
+
+
+    // Add click event to each menu item
+    const menuItems = document.querySelectorAll('.menu a');
+    const checkbox = document.getElementById('check');
+    
+    menuItems.forEach(item => {
+        item.addEventListener('click', (event) => {
+            // Prevent the default link behavior
+            event.preventDefault();
+    
+            // Scroll to the section smoothly
+            const targetId = item.getAttribute('href').substring(1); // Remove '#' to get the ID
+            document.getElementById(targetId)?.scrollIntoView({ behavior: 'smooth' });
+    
+            // Uncheck the checkbox to hide the menu
+            checkbox.checked = false;
+        });
+    });
+    
+    
+
 window.onload = function () {
     var spinner = document.getElementById('spinner');
     
