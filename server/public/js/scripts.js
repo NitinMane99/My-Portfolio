214fbc99abd583eec4473043ad2d21068c91e0ed
Nitin Mane <teraitaservices@gmail.com>
2025-03-26 21:33:08 +0530

    Update scripts.js
    changes in javascript for menu bar

--- server/public/js/scripts.js
+++ server/public/js/scripts.js
@@ -1,35 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('JavaScript is loaded');
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log('JavaScript is loaded');
+// });
 
 
 
-    // Add click event to each menu item
-    const menuItems = document.querySelectorAll('.menu a');
-    const checkbox = document.getElementById('check');
+//     // Add click event to each menu item
+//     const menuItems = document.querySelectorAll('.menu a');
+//     const checkbox = document.getElementById('check');
     
-    menuItems.forEach(item => {
-        item.addEventListener('click', (event) => {
-            // Prevent the default link behavior
-            event.preventDefault();
+//     menuItems.forEach(item => {
+//         item.addEventListener('click', (event) => {
+//             // Prevent the default link behavior
+//             event.preventDefault();
     
-            // Scroll to the section smoothly
-            const targetId = item.getAttribute('href').substring(1); // Remove '#' to get the ID
-            document.getElementById(targetId)?.scrollIntoView({ behavior: 'smooth' });
+//             // Scroll to the section smoothly
+//             const targetId = item.getAttribute('href').substring(1); // Remove '#' to get the ID
+//             document.getElementById(targetId)?.scrollIntoView({ behavior: 'smooth' });
     
-            // Uncheck the checkbox to hide the menu
-            checkbox.checked = false;
+//             // Uncheck the checkbox to hide the menu
+//             checkbox.checked = false;
+//         });
+//     });
+    
+    
+
+// window.onload = function () {
+//     var spinner = document.getElementById('spinner');
+    
+//     // Simulate loading time (you can remove this in real projects)
+//     setTimeout(function () {
+//         spinner.classList.add('spinner-hidden'); // Hide the spinner after content loads
+//     }, 1000); // Adjust time as needed for your loading situation
+// };
+
+document.querySelectorAll(".menu a").forEach(item => {
+            item.addEventListener("click", () => {
+                document.getElementById("check").checked = false;
+            });
         });
-    });
-    
-    
-
-window.onload = function () {
-    var spinner = document.getElementById('spinner');
-    
-    // Simulate loading time (you can remove this in real projects)
-    setTimeout(function () {
-        spinner.classList.add('spinner-hidden'); // Hide the spinner after content loads
-    }, 1000); // Adjust time as needed for your loading situation
-};
 
